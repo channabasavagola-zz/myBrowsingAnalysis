6da443128e277ab5b1d7a49f39cfa3cc5bf2dff6
Channabasava Gola <channuisalways@gmail.com>
2018-02-20 22:43:12 -0700

    Remove pie

--- js/pie.js
+++ js/pie.js
@@ -8,11 +8,6 @@ var margin = {top: 20, right: 20, bottom: 20, left: 20},
 var color = d3.scaleOrdinal()
     .range(["#BBDEFB", "#90CAF9", "#64B5F6", "#42A5F5", "#2196F3", "#1E88E5", "#1976D2"]);
 
-// pie chart arc. Need to create arcs before generating pie
-var arc = d3.arc()
-    .outerRadius(radius - 10)
-    .innerRadius(0);
-
 // donut chart arc
 var arc2 = d3.arc()
     .outerRadius(radius - 10)
@@ -27,13 +22,6 @@ var labelArc = d3.arc()
 var pie = d3.pie()
     .sort(null)
     .value(function(d) { return d.count; });
-
-// define the svg for pie chart
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-  .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
 // define the svg donut chart
 var svg2 = d3.select("body").append("svg")
@@ -51,34 +39,8 @@ d3.csv("data.csv", function(error, data) {
         d.count = +d.count;
         d.domainName = d.domainName;
     })
-
+  
   // "g element is a container used to group other SVG elements"
-  var g = svg.selectAll(".arc")
-      .data(pie(data))
-    .enter().append("g")
-      .attr("class", "arc");
-
-  // append path 
-  g.append("path")
-      .attr("d", arc)
-      .style("fill", function(d) { return color(d.data.domainName); })
-    // transition 
-    .transition()
-      .ease(d3.easeLinear)
-      .duration(2000)
-      .attrTween("d", tweenPie);
-        
-  // append text
-  g.append("text")
-    .transition()
-      .ease(d3.easeLinear)
-      .duration(2000)
-    .attr("transform", function(d) { return "translate(" + labelArc.centroid(d) + ")"; })
-      .attr("dy", ".35em")
-      .text(function(d) { return d.data.domainName; });
-    
-
-    // "g element is a container used to group other SVG elements"
   var g2 = svg2.selectAll(".arc2")
       .data(pie(data))
     .enter().append("g")
